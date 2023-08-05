fe7caf0e0150a5bfe229093135af7e4e27ae65e2
JMY00n <3zlll0@naver.com>
2023-08-05 14:07:33 +0900

    테스트

---
+++ campingapp/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id ("kotlin-kapt")
+    // Add the dependency for the Google services Gradle plugin
+//    id("com.google.gms.google-services") version "4.3.15" apply false
+//    id 'kotlin-kapt'
+
+}
+
+android {
+    namespace = "com.camp.campingapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.camp.campingapp"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // dex 파일 갯수 제한을 풀어주는 코드.
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility=JavaVersion.VERSION_17
+        targetCompatibility=JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget=JavaVersion.VERSION_17.toString()
+    }
+
+    viewBinding {
+        enable = true
+    }
+}
+
+dependencies {
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    implementation ("androidx.multidex:multidex:2.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:compiler:4.12.0")
+
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
+    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}

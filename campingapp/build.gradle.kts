1155c1be197adb38ca5a4e6620a918df56283c3a
JMY00n <3zlll0@naver.com>
2023-08-06 22:31:07 +0900

    게시글 등록하기 버튼 누르면 데이터 저장 됨

--- campingapp/build.gradle.kts
+++ campingapp/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.android")
     id ("kotlin-kapt")
     // Add the dependency for the Google services Gradle plugin
 //    id("com.google.gms.google-services") version "4.3.15" apply false
@@ -50,22 +50,26 @@ android {
 
 dependencies {
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
+    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
 
     implementation ("androidx.multidex:multidex:2.0.1")
 
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.google.android.gms:play-services-auth:20.6.0")
 
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.7.0")
-    implementation("com.firebaseui:firebase-ui-storage:8.0.0")
-    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- campingapp/src/main/java/com/camp/campingapp/MainActivity.kt
+++ campingapp/src/main/java/com/camp/campingapp/MainActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.camp.campingapp.databinding.ActivityMainBinding
 import com.camp.campingapp.model.ItemData
 import com.camp.campingapp.recycler.MyAdapter
+import com.camp.campingapp.util.myCheckPermission
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -18,28 +20,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.wrtie.setOnClickListener {
-            startActivity(Intent(this, WriteActivity::class.java))
+        myCheckPermission(this@MainActivity)
+
+        binding.add.setOnClickListener {
+            startActivity(Intent(this@MainActivity, WriteActivity::class.java))
         }
 
     }
 
-    private fun makeRecyclerView() {
-        MyApplication.db.collection("news").get()
-            .addOnSuccessListener { result ->
-                val itemList = mutableListOf<ItemData>()
-                for (document in result) {
-                    val item = document.toObject(ItemData::class.java)
-                    itemList.add(item)
-                }
-                binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
-                binding.mainRecyclerView.adapter = MyAdapter(this, itemList)
-            }
-            .addOnFailureListener{exception ->
-                Log.d("kkang", "error.. getting document..", exception)
-                Toast.makeText(this, "서버 데이터 획득 실패", Toast.LENGTH_SHORT).show()
-
-
-            }
-    }
+//    private fun makeRecyclerView() {
+//        MyApplication.db.collection("news").get()
+//            .addOnSuccessListener { result ->
+//                val itemList = mutableListOf<ItemData>()
+//                for (document in result) {
+//                    val item = document.toObject(ItemData::class.java)
+//                    itemList.add(item)
+//                }
+//                binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
+//                binding.mainRecyclerView.adapter = MyAdapter(this, itemList)
+//            }
+//            .addOnFailureListener{exception ->
+//                Log.d("kkang", "error.. getting document..", exception)
+//                Toast.makeText(this, "서버 데이터 획득 실패", Toast.LENGTH_SHORT).show()
+//
+//
+//            }
+//    }
 }

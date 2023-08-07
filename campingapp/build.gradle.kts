974cc451257574b6b28f466c05e29c51cfaaf596
JMY00n <3zlll0@naver.com>
2023-08-07 13:51:51 +0900

    파이어베이스에 데이터 저장 기능 추가

--- campingapp/build.gradle.kts
+++ campingapp/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 
     implementation ("androidx.multidex:multidex:2.0.1")
 
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.google.android.gms:play-services-auth:20.6.0")
 
